f8c0e7d1b5a7752da395b6f1b310fb4f508b7258
gslavov <georgi.slavov@nl.abnamro.com>
2020-01-03 17:12:05 +0100

    Add InitBinder for removing whitespace from query params

--- src/main/java/com/afkl/cases/df/api/AirportController.java
+++ src/main/java/com/afkl/cases/df/api/AirportController.java
@@ -5,8 +5,11 @@ import com.afkl.cases.df.model.dto.AirportsResponse;
 import com.afkl.cases.df.model.dto.Location;
 import com.afkl.cases.df.travel.TravelApiClient;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +21,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AirportController {
 
     private TravelApiClient travelApiClient;
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+    }
 
     @GetMapping
     public AirportsResponse getAirports(@RequestParam(value = "size", required = false) final Integer size,
